e12ff1a0fffe360af47f8366e57d9652d16ce39d
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2022-04-10 21:59:17 +0300

    New Project

---
+++ HomeWork-25/HomeWork-25/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-25
+//
+//  Created by Ваня Науменко on 10.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
